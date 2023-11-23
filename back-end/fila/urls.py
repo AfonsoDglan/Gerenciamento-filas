ce1adc964a90c78e1b6c496d907282cd7ee9380f
afonso dglan <afonso.dglan@uft.edu.br>
2023-11-23 11:07:53 -0300

    web socket senhas

--- back-end/fila/urls.py
+++ back-end/fila/urls.py
@@ -1,10 +1,17 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path
 from fila.views.senhasViews import SenhaViewSet
+from django.http import HttpResponse
+
 
 router = DefaultRouter()
 router.register("senhas", SenhaViewSet, basename="senha")
 
 urlpatters = router.urls
-urlpatters += [
-       
+
+def teste(request):
+   return HttpResponse("teste")
+urlpatterns = [
+    path("", teste),
 ]
+urlpatterns += router.urls
